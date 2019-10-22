6465ac016fbcea31dd0553723ef79b58c32e02d4
Martin Neundorfer <m.neundorfer@labor.digital>
2019-10-23 00:28:14 +0200

    feat: implement status code bridge to the vue rendering package

--- src/expressSsrPlugin.ts
+++ src/expressSsrPlugin.ts
@@ -41,12 +41,17 @@ function applyMetaData(vueContext, chunk: string): string {
 	return chunk;
 }
 
-let metaDataInjectCache = null;
-
 module.exports = function expressSsrPlugin(context: ExpressContext): Promise<ExpressContext> {
 
+	/**
+	 * Internal helper to recreate the bundle renderer instance when webpack rebuilt the defnition
+	 * @param bundle
+	 * @param template
+	 * @param clientManifest
+	 */
 	function createRenderer(bundle, template, clientManifest?) {
 		return createBundleRenderer(bundle, {
+			runInNewContext: false,
 			template,
 			clientManifest,
 			cache: new LRU({
@@ -56,90 +61,100 @@ module.exports = function expressSsrPlugin(context: ExpressContext): Promise<Exp
 		});
 	}
 
-	return getOutputPath(context).then(outputPath => {
-		let renderer = null;
-		if (context.isProd) {
-			const serverBundle = require(path.resolve(outputPath, "./vue-ssr-server-bundle.json"));
-			const clientManifest = require(path.resolve(outputPath, "./vue-ssr-client-manifest.json"));
+	return getOutputPath(context)
+		.then(outputPath => {
+			let renderer = null;
+			if (context.isProd) {
+				const serverBundle = require(path.resolve(outputPath, "./vue-ssr-server-bundle.json"));
+				const clientManifest = require(path.resolve(outputPath, "./vue-ssr-client-manifest.json"));
 
-			const template = fs.readFileSync(path.resolve(outputPath, "./index.html"), "utf-8");
-			renderer = createRenderer(serverBundle, template, clientManifest);
-		} else {
+				const template = fs.readFileSync(path.resolve(outputPath, "./index.html"), "utf-8");
+				renderer = createRenderer(serverBundle, template, clientManifest);
+			} else {
 
-			let template = null;
-			let bundle = null;
-			let clientManifest = undefined;
+				let template = null;
+				let bundle = null;
+				let clientManifest = undefined;
 
-			// Register global render generation
-			global.EXPRESS_VUE_SSR_UPDATE_RENDERER = (type: "template" | "bundle" | "clientManifest", value: string) => {
-				try {
-					if (type === "template") {
-						template = value;
-						if (bundle !== null) renderer = createRenderer(bundle, template, clientManifest);
-					} else if (type === "bundle") {
-						bundle = value;
-						if (template !== null) renderer = createRenderer(bundle, template, clientManifest);
-					} else if (type === "clientManifest") {
-						clientManifest = JSON.parse(value);
+				// Register global render generation
+				global.EXPRESS_VUE_SSR_UPDATE_RENDERER = (type: "template" | "bundle" | "clientManifest", value: string) => {
+					try {
+						if (type === "template") {
+							template = value;
+							if (bundle !== null) renderer = createRenderer(bundle, template, clientManifest);
+						} else if (type === "bundle") {
+							bundle = value;
+							if (template !== null) renderer = createRenderer(bundle, template, clientManifest);
+						} else if (type === "clientManifest") {
+							clientManifest = JSON.parse(value);
+						}
+					} catch (e) {
+						console.log(e);
+						process.exit(1);
 					}
-				} catch (e) {
-					console.log(e);
-					process.exit(1);
-				}
-			};
+				};
 
-			// Register callback when the compiler is done
-			context.compiler.hooks.done.tap("ExpressSsrPlugin", () => {
-				const mfs: MemoryFileSystem = context.compiler.outputFileSystem as any;
+				// Register callback when the compiler is done
+				context.compiler.hooks.done.tap("ExpressSsrPlugin", () => {
+					const mfs: MemoryFileSystem = context.compiler.outputFileSystem as any;
 
-				// Update the client manifest
-				const clientManifestPath = path.join(outputPath, "vue-ssr-client-manifest.json");
-				if (mfs.existsSync(clientManifestPath))
-					global.EXPRESS_VUE_SSR_UPDATE_RENDERER("clientManifest", mfs.readFileSync(clientManifestPath).toString("utf-8"));
+					// Update the client manifest
+					const clientManifestPath = path.join(outputPath, "vue-ssr-client-manifest.json");
+					if (mfs.existsSync(clientManifestPath))
+						global.EXPRESS_VUE_SSR_UPDATE_RENDERER("clientManifest", mfs.readFileSync(clientManifestPath).toString("utf-8"));
 
-				// Update the template
-				const indexFilePath = path.join(outputPath, "index.html");
-				if (mfs.existsSync(indexFilePath))
-					global.EXPRESS_VUE_SSR_UPDATE_RENDERER("template", mfs.readFileSync(indexFilePath).toString("utf-8"));
+					// Update the template
+					const indexFilePath = path.join(outputPath, "index.html");
+					if (mfs.existsSync(indexFilePath))
+						global.EXPRESS_VUE_SSR_UPDATE_RENDERER("template", mfs.readFileSync(indexFilePath).toString("utf-8"));
+				});
+			}
+
+			// Serve our generated assets
+			context.expressApp.use(context.compiler.options.output.publicPath.replace(/^\./, ""),
+				require("express").static(outputPath));
+
+			// Register the catch all express route
+			context.expressApp.get("*", (req, res) => {
+				if (!renderer) return res.end("Waiting for compilation... Refresh in a moment.");
+				const s = Date.now();
+				res.setHeader("Content-Type", "text/html");
+
+				const errorHandler = err => {
+					if (err && err.code === 404) {
+						res.status(404).end("404 | Page Not Found");
+					} else {
+						// Render Error Page or Redirect
+						res.status(500).end("500 | Internal Server Error");
+						console.error(`Error during render : ${req.url}`);
+						console.error(err);
+					}
+				};
+
+				// Create the rendering stream
+				const vueContext = {url: req.url, status: 200};
+				const stream = renderer.renderToStream(vueContext);
+
+				// Add the status to the response object
+				stream.once("data", () => res.status(vueContext.status));
+
+				// Apply meta data by the "vue-meta" plugin
+				stream.on("data", (chunk: Buffer) => {
+					res.write(applyMetaData(vueContext, chunk.toString("utf-8")));
+				});
+
+				// End the response when the stream ends
+				stream.on("end", () => {
+					console.log(`Request duration: ${Date.now() - s}ms`);
+					res.end();
+				});
+
+				// Handle errors
+				stream.on("error", errorHandler);
 			});
-		}
 
-		// Serve our generated assets
-		context.expressApp.use(context.compiler.options.output.publicPath.replace(/^\./, ""),
-			require("express").static(outputPath));
-
-		// Register the catch all express route
-		context.expressApp.get("*", (req, res) => {
-			if (!renderer) return res.end("waiting for compilation... refresh in a moment.");
-			const s = Date.now();
-			res.setHeader("Content-Type", "text/html");
-
-			const errorHandler = err => {
-				if (err && err.code === 404) {
-					res.status(404).end("404 | Page Not Found");
-				} else {
-					// Render Error Page or Redirect
-					res.status(500).end("500 | Internal Server Error");
-					console.error(`error during render : ${req.url}`);
-					console.error(err);
-				}
-			};
-
-			// Create the rendering stream
-			const vueContext = {url: req.url};
-			const stream = renderer.renderToStream(vueContext);
-
-			// Apply meta data by the "vue-meta" plugin
-			stream.on("data", (chunk: Buffer) => {
-				res.write(applyMetaData(vueContext, chunk.toString("utf-8")));
-			});
-
-			// Handle errors
-			stream.on("error", errorHandler).on("end", () => console.log(`whole request: ${Date.now() - s}ms`));
+			return Promise.resolve(context);
 		});
-
-		return Promise.resolve(context);
-	});
 
 
 };
