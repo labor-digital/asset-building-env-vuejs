483ede24833b4310658d7e5cddecb8826555368b
Martin Neundorfer <m.neundorfer@labor.digital>
2020-04-24 13:33:31 +0200

    fix: add Arrays.include polyfill to make sure your vue-i18n installation won't break in the latest update

--- src/VueJs.ts
+++ src/VueJs.ts
@@ -16,23 +16,23 @@
  * Last modified: 2020.02.17 at 22:21
  */
 
-import {WorkerContext} from "@labor-digital/asset-building/dist/Core/WorkerContext";
-import {AssetBuilderEventList} from "@labor-digital/asset-building/dist/AssetBuilderEventList";
-import merge from "webpack-merge";
-import {VueLoaderPlugin} from "vue-loader";
 import {AssetBuilderConfiguratorIdentifiers} from "@labor-digital/asset-building/dist/AssetBuilderConfiguratorIdentifiers";
-import {AppDefinitionInterface} from "@labor-digital/asset-building/dist/Interfaces/AppDefinitionInterface";
+import {AssetBuilderEventList} from "@labor-digital/asset-building/dist/AssetBuilderEventList";
 import {ProcessManager} from "@labor-digital/asset-building/dist/Core/ProcessManager";
-import path from "path";
+import {WorkerContext} from "@labor-digital/asset-building/dist/Core/WorkerContext";
+import {AppDefinitionInterface} from "@labor-digital/asset-building/dist/Interfaces/AppDefinitionInterface";
 import {md5} from "@labor-digital/helferlein/lib/Misc/md5";
+import path from "path";
+import {VueLoaderPlugin} from "vue-loader";
+import merge from "webpack-merge";
 
 export default function (context: WorkerContext, scope: string) {
 	if (scope !== "app") throw new Error("The vue extension can not be defined on a global scope!");
-
+	
 	// Add our custom configuration
 	context.eventEmitter.bind(AssetBuilderEventList.APPLY_EXTENSION_WEBPACK_CONFIG, (e) => {
 		const context: WorkerContext = e.args.context;
-
+		
 		// Add vue loader
 		context.webpackConfig.module.rules.push({
 			test: /\.vue$/,
@@ -47,7 +47,7 @@ export default function (context: WorkerContext, scope: string) {
 				}
 			}
 		});
-
+		
 		// Disable node module injections
 		context.webpackConfig = merge(context.webpackConfig, {
 			resolve: {
@@ -66,10 +66,10 @@ export default function (context: WorkerContext, scope: string) {
 				child_process: "empty"
 			}
 		});
-
+		
 		// Add vue plugin
 		context.webpackConfig.plugins.push(new VueLoaderPlugin());
-
+		
 		// Add server side rendering configuration if required
 		if (context.app.useSsr || global.EXPRESS_VUE_SSR_MODE === true) {
 			if (context.app.ssrWorker === "server") {
@@ -79,15 +79,15 @@ export default function (context: WorkerContext, scope: string) {
 					// fashion, and also tells `vue-loader` to emit server-oriented code when
 					// compiling Vue components.
 					target: "node",
-
+					
 					// For bundle renderer source map support
 					devtool: "source-map",
-
+					
 					// This tells the server bundle to use Node-style exports
 					output: {
 						libraryTarget: "commonjs2"
 					},
-
+					
 					// https://webpack.js.org/configuration/externals/#function
 					// https://github.com/liady/webpack-node-externals
 					// Externalize app dependencies. This makes the server build much faster
@@ -96,9 +96,9 @@ export default function (context: WorkerContext, scope: string) {
 						// do not externalize dependencies that need to be processed by webpack.
 						// you can add more file types here e.g. raw *.vue files
 						// you should also whitelist deps that modifies `global` (e.g. polyfills)
-						whitelist: /\.css$/
+						whitelist: /\.css$|\.vue$|[\\\/]src[\\\/]|[\\\/]source[\\\/]/
 					})),
-
+					
 					// This is the plugin that turns the entire output of the server build
 					// into a single JSON file. The default file name will be
 					// `vue-ssr-server-bundle.json`
@@ -108,7 +108,7 @@ export default function (context: WorkerContext, scope: string) {
 						new (require("webpack")).DefinePlugin({
 							"process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV === "production" || context.isProd ? "production" : "development"),
 							"process.env.VUE_ENV": "\"server\""
-						}),
+						})
 					]
 				});
 			} else {
@@ -129,10 +129,10 @@ export default function (context: WorkerContext, scope: string) {
 						new (require("webpack")).DefinePlugin({
 							"process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV === "production" || context.isProd ? "production" : "development"),
 							"process.env.VUE_ENV": "\"client\""
-						}),
+						})
 					]
 				});
-
+				
 				// Rewrite output file
 				const hash = md5((new Date()).toTimeString() + "-" + Math.random() + Math.random());
 				context.webpackConfig.output.filename =
@@ -140,25 +140,30 @@ export default function (context: WorkerContext, scope: string) {
 			}
 		}
 	});
-
+	
+	// Add additional polyfills to support vue-i18n
+	context.eventEmitter.bind(AssetBuilderEventList.FILTER_POLYFILLS, (e) => {
+		e.args.polyfills.push("core-js/features/array/includes.js");
+	});
+	
 	// Set jsx factory in typescript
 	context.eventEmitter.bind(AssetBuilderEventList.FILTER_TYPESCRIPT_OPTIONS, (e) => {
 		e.args.options.compilerOptions.jsxFactory = "h";
 	});
-
+	
 	// Listen for the server bundle compiler
 	context.eventEmitter.bind(AssetBuilderEventList.WEBPACK_COMPILER, e => {
 		const context: WorkerContext = e.args.context;
-
+		
 		// Ignore if we are not running in express mode
 		if (!context.parentContext.isExpress) return;
 		if (context.app.ssrWorker !== "server" || typeof context.webpackConfig.output.path !== "string") return;
-
+		
 		// Set the compiler to the memory fs
 		const MFS = require("memory-fs");
 		const mfs = new MFS();
 		e.args.webpackCompiler.compiler.outputFileSystem = mfs;
-
+		
 		// Register the on done hook
 		e.args.webpackCompiler.compiler.hooks.done.tap("ExpressSsrServerPlugin", () => {
 			const bundlePath = path.join(context.webpackConfig.output.path, "vue-ssr-server-bundle.json");
@@ -168,7 +173,7 @@ export default function (context: WorkerContext, scope: string) {
 			});
 		});
 	});
-
+	
 	// Inject our own app schema property
 	context.eventEmitter.bind(AssetBuilderEventList.FILTER_APP_DEFINITION_SCHEMA, (e) => {
 		e.args.schema.useCssExtractPlugin = {
@@ -188,7 +193,7 @@ export default function (context: WorkerContext, scope: string) {
 			default: undefined
 		};
 	});
-
+	
 	// Inject custom template if we are using vue server side rendering
 	context.eventEmitter.bind(AssetBuilderEventList.FILTER_HTML_PLUGIN_TEMPLATE, e => {
 		if (context.app.useSsr !== true && global.EXPRESS_VUE_SSR_MODE !== true) return;
@@ -196,24 +201,24 @@ export default function (context: WorkerContext, scope: string) {
 		e.args.template.minify = false;
 		e.args.template.inject = false;
 	});
-
+	
 	// Check if we have to spawn the SSR renderers
 	context.eventEmitter.bind(AssetBuilderEventList.AFTER_WORKER_INIT_DONE, (e) => {
 		const context: WorkerContext = e.args.context;
-
+		
 		// Make sure the html template is enabled in ssr mode
 		const isSsr = context.app.useSsr === true || global.EXPRESS_VUE_SSR_MODE === true;
 		if (!isSsr) return;
 		if (context.app.htmlTemplate === null || typeof context.app.htmlTemplate === "undefined")
 			context.app.htmlTemplate = true;
-
+		
 		// Ignore if the context is not correct
 		if (typeof context.app.ssrWorker !== "undefined") return;
 		if (context.parentContext.isExpress && context.parentContext.isProd) return;
-
+		
 		// Create our process manager
 		const processManager = new ProcessManager(context.eventEmitter);
-
+		
 		const serverAppConfig: AppDefinitionInterface = JSON.parse(JSON.stringify(context.app));
 		serverAppConfig.appName += " - Server Generator";
 		serverAppConfig.id += 1000;
@@ -224,7 +229,7 @@ export default function (context: WorkerContext, scope: string) {
 		serverAppConfig.verboseResult = true;
 		serverAppConfig.useSsr = true;
 		serverAppConfig.ssrWorker = "server";
-
+		
 		// Add our callback when the process finished
 		const serverProcessListener = (e) => {
 			e.args.process.on("message", message => {
@@ -234,56 +239,54 @@ export default function (context: WorkerContext, scope: string) {
 			});
 		};
 		context.eventEmitter.bind(AssetBuilderEventList.PROCESS_CREATED, serverProcessListener);
-
+		
 		// Make sure to set the correct mode
 		const modeBackup = context.parentContext.mode;
 		context.parentContext.mode = context.parentContext.isProd ? "build" : "watch";
-
+		
 		// Create the server worker
 		processManager.startSingleWorkerProcess(context.parentContext, serverAppConfig)
 			.catch((err) => {
 				if (typeof err === "string") throw new Error(err);
 				throw err;
 			});
-
+		
 		context.eventEmitter.unbind(AssetBuilderEventList.PROCESS_CREATED, serverProcessListener);
 		context.parentContext.mode = modeBackup;
-
+		
 	});
-
+	
 	// Make sure the styles get minified when we are using server side rendering in production mode
 	context.eventEmitter.bind(AssetBuilderEventList.FILTER_POSTCSS_PLUGINS, (e) => {
 		// Ignore if this is neither a ssr app, nor build for production
 		if (context.app.useSsr !== true || !context.isProd) return;
-
+		
 		// Add the css minifier plugin
 		e.args.plugins.push(require("cssnano"));
 	});
-
+	
 	// Make sure we don't run into issues when using the css extract plugin with vue modules
 	context.eventEmitter.bind(AssetBuilderEventList.FILTER_PLUGIN_CONFIG, (e) => {
 		if (e.args.identifier !== AssetBuilderConfiguratorIdentifiers.CSS_EXTRACT_PLUGIN) return;
 		e.args.config.ignoreOrder = true;
 	});
-
+	
 	// Make sure we don't remove the vue-ssr-server-bundle.json in the clean output dir plugin
 	context.eventEmitter.bind(AssetBuilderEventList.FILTER_PLUGIN_CONFIG, (e) => {
 		if (e.args.identifier !== AssetBuilderConfiguratorIdentifiers.CLEAN_OUTPUT_DIR_PLUGIN) return;
 		e.args.config.cleanOnceBeforeBuildPatterns.push("!vue-ssr-server-bundle.json");
 	});
-
+	
 	// Change the style loader to use the vue style loader
 	context.eventEmitter.bind(AssetBuilderEventList.FILTER_LOADER_CONFIG, (e) => {
 		const cssExtractorPluginRegex = new RegExp("mini-css-extract-plugin");
-
-
 		// Register additional loader to strip out all /deep/ selectors we need for component nesting,
 		// but that are not wanted in a browser environment
 		if (e.args.identifier === AssetBuilderConfiguratorIdentifiers.SASS_LOADER ||
 			e.args.identifier === AssetBuilderConfiguratorIdentifiers.LESS_LOADER) {
 			const deepRemoverPath = path.resolve(__dirname, "DeepRemoverLoader.js");
 			e.args.config.use.forEach((v, k) => {
-
+				
 				// Inject vue style loader
 				if (v.loader.match(cssExtractorPluginRegex)) {
 					const before = e.args.config.use.slice(0, k + 1);
@@ -296,33 +299,33 @@ export default function (context: WorkerContext, scope: string) {
 				}
 			});
 		}
-
+		
 		// If we are in production mode and we don't use the server
 		// side renderer we will not inject the vue style loader
 		if (context.app.useSsr !== true && context.isProd) return;
-
+		
 		// Skip if the package requires us to use the css extract plugin
 		if (context.app.useCssExtractPlugin === true) return;
-
+		
 		// Rewrite sass loader
 		if (e.args.identifier === AssetBuilderConfiguratorIdentifiers.SASS_LOADER) {
 			e.args.config.use.forEach((v, k) => {
 				if (typeof v === "string") v = {loader: v};
 				if (typeof v.loader === "undefined") return;
-
+				
 				// Inject vue style loader
 				if (v.loader.match(cssExtractorPluginRegex)) {
 					e.args.config.use[k] = "vue-style-loader";
 				}
 			});
 		}
-
+		
 		// Rewrite less loader
 		if (e.args.identifier === AssetBuilderConfiguratorIdentifiers.LESS_LOADER) {
 			e.args.config.use.forEach((v, k) => {
 				if (typeof v === "string") v = {loader: v};
 				if (typeof v.loader === "undefined") return;
-
+				
 				// Inject vue style loader
 				if (v.loader.match(cssExtractorPluginRegex))
 					e.args.config.use[k] = "vue-style-loader";
